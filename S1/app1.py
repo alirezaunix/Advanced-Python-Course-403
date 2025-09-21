c085134fd1301b4dbc97069d7a9944c0adc7705d
alirezaunix <alirezaunix@gmail.com>
2025-09-21 17:39:36 +0330

    06-30-1

---
+++ S1/app1.py
@@ -0,0 +1,11 @@
+def f1(x,y):
+    print("I am in f1 Function",x+y)
+
+def f2(a,b):
+    print("I am in f2 Function")
+
+
+if __name__=="__main__":
+    print("First Line")
+    f1(y=12,x=15)
+    print("Last Line")
